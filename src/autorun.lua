8cb3cba8c309a48c9e3f0e9f0bd4370e67cdfbe9
Vurv <56230599+Vurv78@users.noreply.github.com>
2021-07-21 18:03:16 -0700

    Security fixes
    We used a lot of global functions which could be detoured.
    
    Also made this compatible with emmylua

--- src/autorun.lua
+++ src/autorun.lua
@@ -7,6 +7,9 @@
 					   /____/
 ]]
 
+---@diagnostic disable:undefined-global
+---@diagnostic disable:undefined-field
+
 -- Files to hide in file reading and writing. If a path contains any of these keywords, it will be blocked. Uses string.find
 local BlockedPaths = {
 	"sf_filedata",
@@ -48,11 +51,13 @@ local BlacklistedNetMessages = {
 	["pac_footstep"] = true
 }
 
-local d_gsub = string.gsub
+local d_stringgsub = string.gsub
 
 --- Doesn't account for \0 but we shouldn't use that in URLs anyway.
+---@param s string
+---@return string pattern_safe
 local function patternSafe(s)
-	return d_gsub(s, "[%(%)%.%%%+%-%*%?%[%]%^%$]", "%%%1")
+	return d_stringgsub(s, "[%(%)%.%%%+%-%*%?%[%]%^%$]", "%%%1")
 end
 
 local function pattern(str) return { str, true } end
@@ -117,9 +122,6 @@ local URLWhitelist = {
 	Detour vgui functions
 ]]
 
--- 8/19/2020
--- 7/7/2021
-
 local ProtectedMetatables = {
 	[_G] = "_G",
 	[debug.getregistry()] = "_R"
@@ -129,22 +131,42 @@ local ProtectedMetatables = {
 local d_format = string.format
 local d_stringfind = string.find
 local d_stringmatch = string.match
-local d_print = print
-local d_getinfo = debug.getinfo
-local d_unpack = _G.unpack
-local d_date = os.date
-local d_traceback = debug.traceback
-local d_rawset = rawset
-local d_jitattach = _G.jit.attach
+local d_stringgmatch = string.gmatch
 
+local d_date = os.date
+
+local d_getinfo = debug.getinfo
+local d_traceback = debug.traceback
+
+local d_rawget = rawget
+local d_rawset = rawset
 local d_setmetatable = debug.setmetatable
 local d_getmetatable = debug.getmetatable
+local d_pairs = pairs
+local d_ipairs = ipairs
 
+local d_type = type
+local d_istable = istable
+local d_isstring = isstring
+local d_isnumber = isnumber
+
+local WORLDSPAWN = game.GetWorld()
 local REGISTRY = debug.getregistry()
 
 local COLOR = {}
 COLOR.__index = COLOR
 
+---@class Color
+---@field r number
+---@field g number
+---@field b number
+---@field a number?
+
+---@param r number
+---@param g number
+---@param b number
+---@param a number?
+---@return Color
 local function Color( r, g, b, a ) -- Mini color function
 	return setmetatable( {r = r, g = g, b = b, a = a or 255}, COLOR )
 end
@@ -175,6 +197,7 @@ local LOGGER = {
 	EVIL = "EVIL"
 }
 
+---@param urgency string
 local function log(urgency, ...)
 	if urgency == LOGGER.EVIL then
 		alert(...)
@@ -183,14 +206,16 @@ local function log(urgency, ...)
 	sautorun.log( d_format("[%s] -> ", urgency) .. d_format(...) .. " -> " .. ( getLocation(6) or getLocation(5) or getLocation(4) or getLocation(3) or getLocation(2) ) )
 end
 
+---@param mdl string
 local function isMaliciousModel(mdl)
 	-- https://github.com/Facepunch/garrysmod-issues/issues/4449
 	if d_stringmatch(mdl, ".*%.(.*)") == "bsp" then return true end
 	return false
 end
 
+---@param url string
 local function isWhitelistedURL(url)
-	if not isstring(url) then return false end
+	if not d_isstring(url) then return false end
 
 	local relative = d_stringmatch(url, "^https?://(.*)")
 	if not relative then return false end
@@ -208,7 +233,10 @@ local function isWhitelistedURL(url)
 end
 
 --- Returns the true size of a table, including the size of tables inside of that table.
--- Should be safe from infinite recursion.
+--- Should be safe from infinite recursion.
+---@param t table
+---@param done table?
+---@return number table_size
 local function trueTableSize(t, done)
 	local sz = 0
 	sz = sz + #t
@@ -216,8 +244,8 @@ local function trueTableSize(t, done)
 	for k, v in pairs(t) do
 		if not done[v] then
 			done[v] = true
-			if istable(k) then sz = sz + trueTableSize(k, done) end
-			if istable(v) then sz = sz + trueTableSize(v, done) end
+			if d_istable(k) then sz = sz + trueTableSize(k, done) end
+			if d_istable(v) then sz = sz + trueTableSize(v, done) end
 			done[v] = nil
 		end
 	end
@@ -225,24 +253,29 @@ local function trueTableSize(t, done)
 end
 
 --- Cheap check if two tables equal in their values
+---@param t table
+---@param t2 table
+---@return boolean mostly_copied
 local function isTableMostlyCopied(t, t2)
-	if not istable(t) or not istable(t2) then return false end
+	if not d_istable(t) or not d_istable(t2) then return false end
 	if d_getmetatable(t) ~= d_getmetatable(t2) then return false end
 
-	for k, v in pairs(t) do
+	for k, v in d_pairs(t) do
 		if t2[k] ~= v then return false end
 	end
 	return true
 end
 
 --- Returns a locked version of table t.
+---@param t table
+---@return table locked_version
 local function getLocked(t)
 	return setmetatable({}, {
 		__index = t,
 		__newindex = function(_, k, v)
 			-- Do not allow overwriting, which could cause crashes.
-			if rawget(t, k) == nil then
-				rawset(t, k, v)
+			if d_rawget(t, k) == nil then
+				d_rawset(t, k, v)
 			end
 		end
 	})
@@ -265,9 +298,9 @@ local detours = { list = {}, hide = {} }
 local d_setfenv = _G.setfenv
 
 --- Returns the given hook to replace a function with.
---@param function Target function to hook
---@param function Hook to return
---@return function New hooked function that was given
+---@param target function Func to hook
+---@param replace_with function Func to return
+---@return function hooked Hooked function that was given in ``replace_with``
 function detours.attach( target, replace_with )
 	local env = setmetatable({ __undetoured = target },{
 		__index = _G,
@@ -279,9 +312,9 @@ function detours.attach( target, replace_with )
 end
 
 --- Returns the original function that the function given hooked.
---@param function Hooked
---@return function Original function, to overwrite with.
---@return bool True if successful.
+---@param hooked function Hooked function
+---@return function original Original function to overwrite with.
+---@return boolean @True if the hook was detached
 function detours.detach( hooked )
 	local ret = detours.list[hooked]
 	detours.list[hooked] = nil
@@ -290,9 +323,9 @@ end
 
 --- Returns the unhooked function if val is hooked.
 -- Else returns ``val``
--- @param function val Function to check. Can actually be any type though.
--- @return function Return function/val
--- @return boolean Was the value hooked?
+---@param val function Function to check. Can actually be any type though.
+---@return function original Unhooked value or function.
+---@return boolean #Was the value hooked?
 function detours.shadow( val )
 	local v = detours.list[val]
 	if v then
@@ -324,7 +357,7 @@ _G.mesh.Begin = detours.attach(mesh.Begin, function(mesh, primitiveType, primite
 end)
 
 _G.RunConsoleCommand = detours.attach(RunConsoleCommand, function(command, ...)
-	for _, blacklisted in ipairs(BlacklistedConCommands) do
+	for _, blacklisted in d_ipairs(BlacklistedConCommands) do
 		if d_stringfind(command, blacklisted) then
 			return log( LOGGER.WARN, "Found blacklisted cmd ['%s'] being executed with RunConsoleCommand", command )
 		end
@@ -335,30 +368,30 @@ end)
 
 -- Behaves exactly like table.insert
 local function pureLuaInsert(tab, key, val)
-	if not istable(tab) then return end
+	if not d_istable(tab) then return end
 
 	if val~=nil then
-		if not isnumber(key) then return end
+		if not d_isnumber(key) then return end
 		for k = #tab, key, -1 do
 			tab[k+1] = tab[k]
 		end
-		rawset(tab, key, val)
+		d_rawset(tab, key, val)
 		return key
 	elseif key ~= nil then
 		local ind = #tab+1
-		rawset(tab, ind, key)
+		d_rawset(tab, ind, key)
 		return ind
 	end
 end
 
 -- This doesn't call the __newindex metamethod, so we need to patrol this func as well.
 _G.table.insert = detours.attach(table.insert, function(myTable, key, value)
-	if myTable and istable(myTable) then
+	if myTable and d_istable(myTable) then
 		if #myTable > 100000 then log(LOGGER.WARN, "Table") return end
 		if trueTableSize(myTable) > 100000 then return log(LOGGER.WARN, "trueTableSize in table.insert was too large!") end
-		if istable(value) and isTableMostlyCopied(myTable, value) then return log(LOGGER.WARN, "Copied table found in table.insert, lag/crash attempt?") end
+		if d_istable(value) and isTableMostlyCopied(myTable, value) then return log(LOGGER.WARN, "Copied table found in table.insert, lag/crash attempt?") end
 	end
-	if value~=nil and isnumber(key) and key > 2^31-1 then return log(LOGGER.WARN, "table.insert with massive key!") end
+	if value~=nil and d_isnumber(key) and key > 2^31-1 then return log(LOGGER.WARN, "table.insert with massive key!") end
 	return pureLuaInsert(myTable, key, value)
 end)
 
@@ -375,7 +408,7 @@ _G.debug.getlocal = detours.attach(debug.getlocal, function(thread, level, index
 end)
 
 _G.tostring = detours.attach(tostring, function(value)
-	if type(value) == "CSoundPatch" then return false, "CSoundPatch" end
+	if d_type(value) == "CSoundPatch" then return "CSoundPatch" end
 	-- Prevent tostring crash with tostring(CreateSound(LocalPlayer(),''))
 	return __undetoured( detours.shadow(value) )
 end)
@@ -414,7 +447,7 @@ end)
 
 local JitCallbacks = {}
 
-_G.jit.attach = detours.attach(jit.attach, function(callback,event)
+_G.jit.attach = detours.attach(jit.attach, function(callback, event)
 	-- Attaches jit to a function so they can get constants and protos from it. We will give it the original function :)
 	JitCallbacks[callback] = event
 	return __undetoured( detours.shadow(callback), event )
@@ -541,7 +574,7 @@ _G.debug.getregistry = detours.attach(debug.getregistry, function()
 	return LOCKED_REGISTRY
 end)
 
-_G.debug.getupvalue = detours.attach(debug.getupvalue, function(func,index)
+_G.debug.getupvalue = detours.attach(debug.getupvalue, function(func, index)
 	return __undetoured( detours.shadow(func), index )
 end)
 
@@ -589,6 +622,8 @@ _G.cam.End = detours.attach( cam.End, popCam() )
 _G.cam.EndOrthoView = detours.attach( cam.EndOrthoView, popCam() )
 
 -- File Metatable, but you can't write with it.
+---@param ret any Value to return inside of function ``f``
+---@return function f File method function that will just log and return the value passed.
 local function fileMethod(ret)
 	return function()
 		log(LOGGER.WARN, "Someone tried to read/write to a locked file!")
@@ -596,6 +631,8 @@ local function fileMethod(ret)
 	end
 end
 
+---@param path string Path to check
+---@return boolean #If the file at the path is not allowed to be accessed.
 local function isLockedPath(path)
 	for _, blocked_path in ipairs(BlockedPaths) do
 		if d_stringfind(path, blocked_path) then
@@ -669,7 +706,7 @@ _G.net.Start = detours.attach(net.Start, function(str, unreliable)
 	return __undetoured(str,unreliable)
 end)
 
-_G.string.rep = detours.attach(string.rep, function(str,reps,separator)
+_G.string.rep = detours.attach(string.rep, function(str, reps, separator)
 	-- Max string.rep length is 1,000,000
 	local sep = separator or ""
 	local retlength = #str*reps + #sep*reps
@@ -677,26 +714,26 @@ _G.string.rep = detours.attach(string.rep, function(str,reps,separator)
 	return __undetoured(str,reps,sep)
 end)
 
-_G.ClientsideModel = detours.attach(ClientsideModel, function(model,rendergroup)
-	if not isstring(model) then return end
+_G.ClientsideModel = detours.attach(ClientsideModel, function(model, rendergroup)
+	if not d_isstring(model) then return end
 	if isMaliciousModel(model) then log(LOGGER.EVIL, "Someone tried to create a ClientsideModel with a .bsp model!") return end
 	return __undetoured(model,rendergroup)
 end)
 
-_G.ClientsideScene = detours.attach(ClientsideScene, function(model,targetEnt)
-	if not isstring(model) then return end
+_G.ClientsideScene = detours.attach(ClientsideScene, function(model, targetEnt)
+	if not d_isstring(model) then return end
 	if isMaliciousModel(model) then log(LOGGER.EVIL, "Someone tried to create a ClientsideScene with a .bsp model!") return end
 	return __undetoured(model,targetEnt)
 end)
 
-_G.ClientsideRagdoll = detours.attach(ClientsideRagdoll, function(model,rendergroup)
-	if not isstring(model) then return end
+_G.ClientsideRagdoll = detours.attach(ClientsideRagdoll, function(model, rendergroup)
+	if not d_isstring(model) then return end
 	if isMaliciousModel(model) then log(LOGGER.EVIL, "Someone tried to create a ClientsideRagdoll with a .bsp model!") return end
 	return __undetoured(model,rendergroup)
 end)
 
 _G.ents.CreateClientProp = detours.attach(ents.CreateClientProp, function(model)
-	if not isstring(model) then model = "models/error.mdl" end
+	if not d_isstring(model) then model = "models/error.mdl" end
 	if isMaliciousModel(model) then log(LOGGER.EVIL, "Someone tried to create a malicious clientsideprop with a .bsp model!") return end
 	return __undetoured(model)
 end)
@@ -749,14 +786,14 @@ _G.AddConsoleCommand = detours.attach(AddConsoleCommand, function(name, helpText
 		return log( LOGGER.EVIL, "Someone tried to crash by adding 'sendrcon' as a concmd" )
 	end
 
-	__undetoured(name,helpText,flags)
+	__undetoured(name, helpText, flags)
 end)
 
 _G.os.date = detours.attach(os.date, function(format, time)
 	-- https://github.com/thegrb93/StarfallEx/blob/423e75182d3590bb1560677d54724c134625b549/lua/starfall/libs_sh/builtins.lua#L348
 	if format ~= nil then
-		for v in string.gmatch(format, "%%(.?)") do
-			if not string.match(v, "[%%aAbBcCdDSHeUmMjIpwxXzZyY]") then
+		for v in d_stringgmatch(format, "%%(.?)") do
+			if not d_stringmatch(v, "[%%aAbBcCdDSHeUmMjIpwxXzZyY]") then
 				return log(LOGGER.EVIL, "Blocked evil os.date format!")
 			end
 		end
@@ -773,22 +810,22 @@ _G.sound.PlayURL = detours.attach(sound.PlayURL, function(url, flags, callback)
 	return __undetoured(url, flags, callback)
 end)
 
-_G.HTTP = detours.attach(HTTP, function(params)
-	if not params then return end -- todo: investigate if this should error instead
-	if not isWhitelistedURL(params.url) then
-		return log(LOGGER.INFO, "Blocked HTTP Request to %s", params.url)
+_G.HTTP = detours.attach(HTTP, function(parameters)
+	if not parameters then return end -- todo: investigate if this should error instead
+	if not isWhitelistedURL(parameters.url) then
+		return log(LOGGER.INFO, "Blocked HTTP Request to %s", parameters.url)
 	end
-	return __undetoured(params)
+	return __undetoured(parameters)
 end)
 
 -- Registry detours
 REGISTRY.Player.ConCommand = detours.attach(REGISTRY.Player.ConCommand, function(ply, command)
-	if not ply or not isstring(command) then return end
+	if not ply or not d_isstring(command) then return end
 
 	log( LOGGER.INFO, "%s:ConCommand(%s)", ply, command )
 
 	local args = {}
-	for arg in string.gmatch(command, "[^%s]+") do
+	for arg in d_stringgmatch(command, "[^%s]+") do
 		args[#args+1] = arg
 	end
 
@@ -803,7 +840,7 @@ REGISTRY.Entity.SetModel = detours.attach(REGISTRY.Entity.SetModel, function(sel
 end)
 
 REGISTRY.Entity.DrawModel = detours.attach(REGISTRY.Entity.DrawModel, function(self, flags)
-	if self == game.GetWorld() then
+	if self == WORLDSPAWN then
 		return log(LOGGER.EVIL, "Entity:DrawModel() called with worldspawn") -- Crash
 	end
 	__undetoured(self, flags)
